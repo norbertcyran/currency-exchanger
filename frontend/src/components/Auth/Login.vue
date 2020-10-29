f9a215ab391096ddcffab3405776bbd6f57f9278
Norbert Cyran <cyran.norbert97@gmail.com>
2020-10-29 23:42:03 +0100

    Apply linters

--- frontend/src/components/Auth/Login.vue
+++ frontend/src/components/Auth/Login.vue
@@ -1,65 +1,64 @@
 <template>
-    <v-container fill-height >
-        <v-layout align-center justify-center>
-            <v-flex xs12 sm8 md6>
-                <v-form >
-                    <v-card class="elevation-12">
-                        <v-toolbar dark color="blue">
-                            <v-toolbar-title>
-                            Login
-                            </v-toolbar-title>
-                        </v-toolbar>
-                        <v-card-text>
-                            <v-text-field
-                            prepend-icon="mdi-account"
-                            name="login"
-                            label="Login"
-                            type="text"
-                            v-model="email"
-                            >
-
-                            </v-text-field>
-                            <v-text-field
-                            prepend-icon="mdi-lock"
-                            name="password"
-                            label="Password"
-                            type="password"
-                            v-model="password"                            >
-                            </v-text-field>
-                        </v-card-text>
-                        <v-divider light></v-divider>
-                        <v-card-actions>
-                            <v-btn to="/signup" rounded color="indigo" dark>Sign up</v-btn>
-                            <v-spacer></v-spacer>
-                            <v-btn rounded color="indigo" dark  @click.native="login" >Login
-                                <v-icon>mdi-arrow-right</v-icon>
-                            </v-btn>
-                        </v-card-actions>
-                    </v-card>
-                </v-form>
-            </v-flex>
-        </v-layout>
-    </v-container>
-
+  <v-container fill-height>
+    <v-layout align-center justify-center>
+      <v-flex xs12 sm8 md6>
+        <v-form>
+          <v-card class="elevation-12">
+            <v-toolbar dark color="blue">
+              <v-toolbar-title>
+                Login
+              </v-toolbar-title>
+            </v-toolbar>
+            <v-card-text>
+              <v-text-field
+                prepend-icon="mdi-account"
+                name="login"
+                label="Login"
+                type="text"
+                v-model="email"
+              >
+              </v-text-field>
+              <v-text-field
+                prepend-icon="mdi-lock"
+                name="password"
+                label="Password"
+                type="password"
+                v-model="password"
+              >
+              </v-text-field>
+            </v-card-text>
+            <v-divider light></v-divider>
+            <v-card-actions>
+              <v-btn to="/signup" rounded color="indigo" dark>Sign up</v-btn>
+              <v-spacer></v-spacer>
+              <v-btn rounded color="indigo" dark @click.native="login"
+                >Login
+                <v-icon>mdi-arrow-right</v-icon>
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-form>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 <script>
 export default {
-    name:'login',
-    data:()=>({
-        email : "",
-        password : ""
-      }),
-    methods: {
-      login() {
-          console.log("ja")
-        let email = this.email
-        let password = this.password
-        this.$store.dispatch('login', { email, password })
-       .then(() => this.$router.push('/'))
-       .catch(err => console.log(err))
-      }
+  name: "login",
+  data: () => ({
+    email: "",
+    password: ""
+  }),
+  methods: {
+    login() {
+      console.log("ja");
+      let email = this.email;
+      let password = this.password;
+      this.$store
+        .dispatch("login", { email, password })
+        .then(() => this.$router.push("/"))
+        .catch(err => console.log(err));
     }
-}
-
-
+  }
+};
 </script>
--- frontend/src/components/Auth/Signup.vue
+++ frontend/src/components/Auth/Signup.vue
@@ -1,17 +1,16 @@
 <template>
-    <v-container fluid fill-height >
-        <v-layout align-center justify-center>
-            <v-flex xs12 sm8 md6>
-                
-                    <v-card class="elevation-12">
-                        <v-toolbar dark color="blue">
-                            <v-toolbar-title>
-                            Sign up
-                            </v-toolbar-title>
-                        </v-toolbar>
-                        <v-card-text>
-                            <v-form>
-                                <!-- <v-alert
+  <v-container fluid fill-height>
+    <v-layout align-center justify-center>
+      <v-flex xs12 sm8 md6>
+        <v-card class="elevation-12">
+          <v-toolbar dark color="blue">
+            <v-toolbar-title>
+              Sign up
+            </v-toolbar-title>
+          </v-toolbar>
+          <v-card-text>
+            <v-form>
+              <!-- <v-alert
                                 :value="true"
                                 color="error"
                                 icon="mdi-alert"
@@ -19,114 +18,103 @@
                                 This user already exist, try a diffrent one
                                 </v-alert> -->
 
-                            <v-text-field
-                            prepend-icon="mdi-account"
-                            name="login"
-                            label="Login"
-                            v-model="nick"
-                            :rules="[rules.required]"
-                            >
-                            </v-text-field> 
+              <v-text-field
+                prepend-icon="mdi-account"
+                name="login"
+                label="Login"
+                v-model="nick"
+                :rules="[rules.required]"
+              >
+              </v-text-field>
 
-                            <v-text-field
-                            prepend-icon="mdi-email"
-                            name="email"
-                            label="Email"
-                            v-model="email"
-                            :rules="[rules.required, rules.email]"
-                            >
-                            </v-text-field> 
+              <v-text-field
+                prepend-icon="mdi-email"
+                name="email"
+                label="Email"
+                v-model="email"
+                :rules="[rules.required, rules.email]"
+              >
+              </v-text-field>
 
-                            <v-text-field
-                            prepend-icon="mdi-lock"
-                            name="password"
-                            label="Password"
-                            :rules="[rules.required]"
-                            type="password"
-                            :error="valid_length()"
-                            v-model="password"
-                            >
-                            </v-text-field> 
-                            
-                            <v-text-field
-                            prepend-icon="mdi-lock-alert"
-                            name="password"
-                            label="Confirm Password"
-                            :rules="[rules.required]"
-                            type="password"
-                            :error="!valid()"
-                            v-model="confirm_password"
-                            >
-                            </v-text-field> 
-                            
-                            <v-divider light></v-divider>
-                            
-                            </v-form>
-                        </v-card-text>
+              <v-text-field
+                prepend-icon="mdi-lock"
+                name="password"
+                label="Password"
+                :rules="[rules.required]"
+                type="password"
+                :error="valid_length()"
+                v-model="password"
+              >
+              </v-text-field>
 
-                        <v-card-actions>
-                            <v-btn to="/login" round color="black" dark>Sign in</v-btn>
-                            <v-spacer></v-spacer>
-                            <v-btn roun color="success" dark @click.native="register" >Register
-                                <v-icon>mdi-chevron-up</v-icon>
-                            </v-btn>
-                        </v-card-actions>
-                    </v-card>
-               
-            </v-flex>
-        </v-layout>
-    </v-container>
+              <v-text-field
+                prepend-icon="mdi-lock-alert"
+                name="password"
+                label="Confirm Password"
+                :rules="[rules.required]"
+                type="password"
+                :error="!valid()"
+                v-model="confirm_password"
+              >
+              </v-text-field>
 
+              <v-divider light></v-divider>
+            </v-form>
+          </v-card-text>
+
+          <v-card-actions>
+            <v-btn to="/login" round color="black" dark>Sign in</v-btn>
+            <v-spacer></v-spacer>
+            <v-btn roun color="success" dark @click.native="register"
+              >Register
+              <v-icon>mdi-chevron-up</v-icon>
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 <script>
 export default {
-    data:() =>({
-        password:'',
-        confirm_password:'',
-        nick:'',
-        email:'',
+  data: () => ({
+    password: "",
+    confirm_password: "",
+    nick: "",
+    email: "",
 
-        rules:{
-            required: value=> !!value || "Required",
-            email: value=>{
-                  const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-                  return re.test(value) || "Invalid e-mail."
-            }
-        }
-    }),
-    computed:{
-        isFormValid:{
-            get:()=>({
-                
-
-
-            })
-        } 
-
-
-    },
-
-
-    methods:{
-        register(){
-            let data = {
-            nick: this.nick,
-            email: this.email,
-            password: this.password,
-            }
-            this.$store.dispatch('register', data)
-        .then(() => this.$router.push('/'))
-        .catch(err => console.log(err))
-        
-
-            
-        },
-        valid(){
-            return this.password===this.confirm_password;
-            },
-        valid_length(){
-            return this.password.length<=8 && this.password.length!=0;
-        }
+    rules: {
+      required: value => !!value || "Required",
+      email: value => {
+        const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(value) || "Invalid e-mail.";
+      }
     }
-}
+  }),
+  computed: {
+    isFormValid: {
+      get: () => ({})
+    }
+  },
+
+  methods: {
+    register() {
+      let data = {
+        nick: this.nick,
+        email: this.email,
+        password: this.password
+      };
+      this.$store
+        .dispatch("register", data)
+        .then(() => this.$router.push("/"))
+        .catch(err => console.log(err));
+    },
+    valid() {
+      return this.password === this.confirm_password;
+    },
+    valid_length() {
+      return this.password.length <= 8 && this.password.length != 0;
+    }
+  }
+};
 </script>
