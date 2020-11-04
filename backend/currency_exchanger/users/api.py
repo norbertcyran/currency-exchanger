80565fd6fb6c054d8ad1b7068f4eaaf91f3d88c9
FractialCopper <35343054+FractialCopper@users.noreply.github.com>
2020-11-04 23:49:52 +0100

    Created user default crud (#7)
    * Basic cr
    
    * nested serializers
    
    * Applied changes to update and readonly fields
    
    Co-authored-by: Norbert Cyran <cyran.norbert97@gmail.com>

---
+++ backend/currency_exchanger/users/api.py
@@ -0,0 +1,6 @@
+from rest_framework import routers
+from . import views
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewset)
+
--- backend/currency_exchanger/users/views.py
+++ backend/currency_exchanger/users/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
 
+from . import models
+from . import serializers
+from rest_framework import viewsets, status
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework import permissions
 # Create your views here.
+
+
+class UserViewset(viewsets.ModelViewSet):
+    queryset = models.User.objects.all()
+    serializer_class = serializers.UserSerializer
+
+
+
+
+
+
+
+
+
