b0a95e039b9f3277d4f3ed4abe0f06cb4722bf86
Norbert Cyran <cyran.norbert97@gmail.com>
2020-11-10 18:49:02 +0100

    Home page (#11)
    * Add home page
    
    * Add lock
    
    * Connect home page with auth pages
    
    * Delete HelloWorld component

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,35 +1,34 @@
 <template>
   <v-app>
     <v-app-bar app color="primary" dark>
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
+      <router-link to="/">
+        <div class="d-flex align-center">
+          <v-img
+            alt="Vuetify Logo"
+            class="shrink mr-2"
+            contain
+            src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+            transition="scale-transition"
+            width="40"
+          />
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
+          <v-img
+            alt="Vuetify Name"
+            class="shrink mt-1 hidden-sm-and-down"
+            contain
+            min-width="100"
+            src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
+            width="100"
+          />
+        </div>
+      </router-link>
 
       <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
+      <v-btn v-if="isAuthenticated" @click="logout()" text>
+        <span class="mr-2">Logout</span>
+      </v-btn>
+      <v-btn v-else to="/login" text>
+        <span class="mr-2">Login</span>
       </v-btn>
     </v-app-bar>
 
@@ -39,6 +38,8 @@
   </v-app>
 </template>
 <script>
+import { mapActions, mapGetters } from "vuex";
+
 export default {
   name: "App",
 
@@ -46,6 +47,14 @@ export default {
 
   data: () => ({
     //
-  })
+  }),
+
+  computed: {
+    ...mapGetters(["isAuthenticated"])
+  },
+
+  methods: {
+    ...mapActions(["logout"])
+  }
 };
 </script>
