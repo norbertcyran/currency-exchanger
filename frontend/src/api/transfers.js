77c7de0637825e592248b1f70830c39e27f5add3
Norbert Cyran <cyran.norbert97@gmail.com>
2020-12-10 00:10:46 +0100

    Implement transfers on the frontend

--- frontend/src/api/transfers.js
+++ frontend/src/api/transfers.js
@@ -2,10 +2,10 @@ import Vue from "vue";
 
 export default {
   async getTransfers() {
-    return Vue.axios.get("/transfers/");
+    return Vue.axios.get("/api/transfers/");
   },
 
   async sendTransfer(data) {
-    return Vue.axios.post(/transfers/, data);
+    return Vue.axios.post("/api/transfers/", data);
   }
 };
