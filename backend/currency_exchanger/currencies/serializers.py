729fc730b45782513dc86f51d89ff0112903842c
FractialCopper <35343054+FractialCopper@users.noreply.github.com>
2020-12-21 12:41:38 +0100

    #18-cash-in (#54)
    * Implemented cash in
    
    * Implemented cash in
    
    * w/e
    
    * Update signals.py
    
    * cash in
    
    * Update WalletCurrency.vue
    
    * Update WalletCurrency.vue
    
    * repaired api links
    
    * Update backend/currency_exchanger/currencies/serializers.py
    
    Co-authored-by: Norbert Cyran <cyran.norbert97@gmail.com>
    
    * Update frontend/src/api/stocks.js
    
    Co-authored-by: Norbert Cyran <cyran.norbert97@gmail.com>
    
    * removed pyproject and wrapped in async/await
    
    * fixed api call
    
    Co-authored-by: Norbert Cyran <cyran.norbert97@gmail.com>

--- backend/currency_exchanger/currencies/serializers.py
+++ backend/currency_exchanger/currencies/serializers.py
@@ -2,6 +2,7 @@ from currency_exchanger.currencies.models import (
     Currency,
     CurrencyExchange,
     CurrencyHistory,
+    CurrencyTransfer,
     WalletCurrency,
 )
 from currency_exchanger.validators import validate_transaction
@@ -49,3 +50,13 @@ class CurrencyExchangeSerializer(serializers.ModelSerializer):
         validate_transaction(wallet, currency, attrs["amount"])
 
         return attrs
+
+
+class CurrencyTransferSerializer(serializers.ModelSerializer):
+    currency = serializers.SlugRelatedField(
+        many=False, slug_field="code", queryset=Currency.objects.all()
+    )
+
+    class Meta:
+        model = CurrencyTransfer
+        fields = ("id", "currency", "amount")
--- backend/currency_exchanger/urls.py
+++ backend/currency_exchanger/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from .currencies.views import CurrencyExchangeViewSet, CurrencyViewSet
+from .currencies.views import CurrencyExchangeViewSet, CurrencyTransferViewSet, CurrencyViewSet
 from .stocks.views import StockViewSet
 from .transfers.views import MoneyTransferViewSet
 from .users.views import UserViewSet
@@ -28,6 +28,7 @@ router.register(r"users", UserViewSet)
 router.register(r"currencies", CurrencyViewSet)
 router.register(r"exchange", CurrencyExchangeViewSet, basename="exchanges")
 router.register(r"stocks", StockViewSet)
+router.register(r"cashin", CurrencyTransferViewSet, basename="cashin")
 router.register(r"transfers", MoneyTransferViewSet, basename="transfers")
 
 urlpatterns = [
