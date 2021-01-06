67d6a7e564ae373dab0f5d989e56dc5d04e940be
Norbert Cyran <cyran.norbert97@gmail.com>
2021-01-06 16:43:02 +0100

    Allow adding other currencies (#64)
    * Allow adding other currencies than owned
    
    * Lint and cleanup

--- frontend/src/api/wallet.js
+++ frontend/src/api/wallet.js
@@ -2,4 +2,6 @@ import Vue from "vue";
 
 export default {
   getWallet: async () => Vue.axios.get("/api/wallet/"),
+  cashIn: async (currency, amount) =>
+    Vue.axios.post("/api/cashin/", { currency, amount })
 };
--- frontend/src/components/ProfileTransactions/TransferCard.vue
+++ frontend/src/components/ProfileTransactions/TransferCard.vue
@@ -1,15 +1,23 @@
-<template >
-  <v-card  display="block" class="mb-4">
+<template>
+  <v-card display="block" class="mb-4">
     <v-card-title class="body-2"> Money transfer </v-card-title>
     <v-card-text
       >Title: {{ title }}
       <div v-if="isOutgoing">
         To: {{ this.otherUser }}
-      <div class="float-right"> <strong class="red--text text--lighten-1">  -{{ this.amount.toFixed(2) }} {{this.shortcut}}</strong></div>
+        <div class="float-right">
+          <strong class="red--text text--lighten-1">
+            -{{ this.amount.toFixed(2) }} {{ this.shortcut }}</strong
+          >
+        </div>
       </div>
       <div v-else>
         From: {{ this.otherUser }}
-   <div class="float-right"> <strong class="green--text text--lighten-1">  {{ this.amount.toFixed(2) }} {{this.shortcut}}</strong></div>
+        <div class="float-right">
+          <strong class="green--text text--lighten-1">
+            {{ this.amount.toFixed(2) }} {{ this.shortcut }}</strong
+          >
+        </div>
       </div>
     </v-card-text>
 
@@ -17,44 +25,35 @@
   </v-card>
 </template>
 
-
 <script>
-import { currenciesAndShortcuts } from '../../global constant variables/currencies and shortcuts'
 export default {
   props: {
     title: {
-      type: String,
+      type: String
     },
     isOutgoing: {
-      type: Boolean,
+      type: Boolean
     },
     otherUser: {
-      type: String,
+      type: String
     },
     amount: {
-      type: Number,
+      type: Number
     },
     currency: {
-      type: String,
+      type: String
     },
-    time:{
-
-    }
+    time: {}
   },
   computed: {
-    shortcut: function () {
+    shortcut: function() {
       return this.getShortcut(this.currency);
-    },
+    }
   },
-    methods: {
-      getShortcut(currency){
-        console.log(currency)
-        console.log(currenciesAndShortcuts)
-        for(const elem in currenciesAndShortcuts){
-
-          if(currenciesAndShortcuts[elem].currency==currency) return currenciesAndShortcuts[elem].shortcut
-        }
-      },
-  },
+  methods: {
+    getShortcut(currency) {
+      console.log(currency);
+    }
+  }
 };
 </script>
